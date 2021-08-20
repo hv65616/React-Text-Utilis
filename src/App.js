17d0618cdc1988bcd68cb2d816029a642a750a5b
Himanshu <hv65616@gmail.com>
2021-08-20 10:44:27 +0530

    Initial Commit Video4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let name = "Himanshu Verma";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>PHONE</li>
+        <li>CONTACT</li>
+      </nav>
+      <h1>{name}</h1>
+      <div className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nemo
+        autem ea numquam eos sequi. Nihil a explicabo odio dolor at praesentium
+        magnam, culpa numquam fugiat? Architecto iste harum illum minus, nemo
+        tenetur fugit obcaecati quod sequi. Accusamus voluptates ab iusto a
+        animi saepe nulla aliquam! Laborum rem iure omnis totam iusto
+        voluptatum. Quidem voluptatum molestiae doloribus voluptatibus ad nam
+        omnis, atque recusandae expedita nisi, magni saepe similique soluta.
+        Incidunt, iure amet non facere, reprehenderit vitae labore architecto
+        eius delectus pariatur totam. Dignissimos et corrupti, exercitationem
+        maxime asperiores totam architecto nulla quisquam sint quibusdam
+        perferendis quos soluta recusandae? Illum, corrupti.
+      </div>
+    </>
   );
 }
 
