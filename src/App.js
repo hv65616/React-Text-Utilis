399ed44960055f4caa7b66dedd5c83bcd77dc55f
Himanshu <hv65616@gmail.com>
2021-08-20 19:59:18 +0530

    Space and all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import TextForm from "./Components/TextForm";
 function App() {
   return (
     <>
-      <NavBar title = "Text-Utilis"></NavBar>
+      <NavBar title="Text-Utilis"></NavBar>
+      <div className="container my-3">
+        <TextForm heading="Enter your text here:-"></TextForm>
+      </div>
     </>
   );
 }
