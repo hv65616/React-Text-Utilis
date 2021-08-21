0c8942e7f73fbd192fed82056be5aa260efca087
Himanshu <hv65616@gmail.com>
2021-08-21 23:31:32 +0530

    Making navbar color black

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function NavBar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
