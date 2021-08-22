11b2eebabffa067a11d045403958a98dc5db4f85
Himanshu <hv65616@gmail.com>
2021-08-22 21:10:49 +0530

    Setting of mode and text mode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import "./App.css";
 // import About from "./Components/About";
 import NavBar from "./Components/NavBar";
 import TextForm from "./Components/TextForm";
+import React, { useState } from "react";
+
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const [modeText, setModeText] = useState("LightMode");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      setModeText("DarkMode");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      setModeText("LightMode");
+      document.body.style.backgroundColor = "white";
+    }
+  };
   return (
     <>
-      <NavBar title="Text-Utilis"></NavBar>
+      <NavBar
+        title="Text-Utilis"
+        mode={mode}
+        toggleMode={toggleMode}
+        modeText={modeText}
+      ></NavBar>
       <div className="container my-3">
-        <TextForm heading="Enter your text here:-"></TextForm>
+        <TextForm heading="Enter your text here:-" mode={mode}></TextForm>
         {/* <About></About> */}
       </div>
     </>
