bdd1827a8492fda6c61d2f388361a29d472bc3c7
Himanshu <hv65616@gmail.com>
2021-08-24 21:57:31 +0530

    Alert Functionality Code

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import TextForm from "./Components/TextForm";
 import React, { useState } from "react";
+import Alert from "./Components/Alert";
 
 function App() {
   const [mode, setMode] = useState("light");
 
   const [modeText, setModeText] = useState("LightMode");
 
+  const [alert, setAlert] = useState(null);
+
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       setModeText("DarkMode");
       document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode Is Enabled", "success");
     } else {
       setMode("light");
       setModeText("LightMode");
       document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Is Enabled", "success");
     }
+  };
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
   };
   return (
     <>
@@ -28,8 +43,13 @@ function App() {
         toggleMode={toggleMode}
         modeText={modeText}
       ></NavBar>
+      <Alert alert={alert}></Alert>
       <div className="container my-3">
-        <TextForm heading="Enter your text here:-" mode={mode}></TextForm>
+        <TextForm
+          heading="Enter your text here:-"
+          mode={mode}
+          showAlert={showAlert}
+        ></TextForm>
         {/* <About></About> */}
       </div>
     </>
