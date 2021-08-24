5d37d897c86df8e4aeff2a29511755d1a4d89dc4
Himanshu <hv65616@gmail.com>
2021-08-24 21:56:25 +0530

    Created Alert.js For Generating Alert

---
+++ src/Components/Alert.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Alert(props) {
+  const capatalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>
+          {capatalize(props.alert.type)} : {props.alert.msg}
+        </strong>
+      </div>
+    )
+  );
+}
+
+export default Alert;
