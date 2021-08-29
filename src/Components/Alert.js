5c7e650d5f85bb5b5247b73a7acd02d4ec96249c
Himanshu <hv65616@gmail.com>
2021-08-29 19:56:01 +0530

    Added Height for prompt

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -6,16 +6,18 @@ function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
-    props.alert && (
-      <div
-        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-        role="alert"
-      >
-        <strong>
-          {capatalize(props.alert.type)} : {props.alert.msg}
-        </strong>
-      </div>
-    )
+    <div style={{ height: "50px" }}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          <strong>
+            {capatalize(props.alert.type)} : {props.alert.msg}
+          </strong>
+        </div>
+      )}
+    </div>
   );
 }
 
