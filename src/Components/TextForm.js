b8e664bb147196e6c6d07df9bf19fba1d70fefbc
Himanshu <hv65616@gmail.com>
2021-08-30 17:13:35 +0530

    Bug Fix

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -92,7 +92,7 @@ export default function TextForm(props) {
         <h1>Your Text Summary</h1>
         <p>
           {
-            text.split(" ").filter((element) => {
+            text.split(/\s+/).filter((element) => {
               return element.length !== 0;
             }).length
           }{" "}
